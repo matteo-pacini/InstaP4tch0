2ca125fbab10ca9e73c808cee458ee70a509253a
Matteo Pacini <ispeakprogramming@gmail.com>
2019-06-03 21:19:42 +0200

    Better script to thin Swift runtime

--- scripts/thin-swift-runtime.sh
+++ scripts/thin-swift-runtime.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-shopt -s globstar
-shopt -s nullglob
-shopt -s dotglob
+set -e
+set -x
 
-for lib in ../Resources/Frameworks/*.dylib
-do
-  libnoext="${lib%.dylib}"
-  lipo -thin armv7 -output "$libnoext.armv7" "$lib"
-  lipo -thin arm64 -output "$libnoext.arm64" "$lib"
-  rm $lib
-  lipo "$libnoext.armv7" "$libnoext.arm64" -output "$lib" -create
-  rm "$libnoext.armv7" "$libnoext.arm64"
+XCODE_PATH="/Applications/Xcode"
+SWIFT_RUNTIME_PATH="${XCODE_PATH}/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos"
+DEST_PATH="Resources/Frameworks"
+
+rm -f "${DEST_PATH}/libswift*.dylib"
+
+find "${SWIFT_RUNTIME_PATH}" -type f -name "*.dylib" -print0 | while IFS= read -r -d $'\0' DYLIB; do
+  DYLIB_NO_EXT="${DYLIB%.dylib}"
+  DYLIB_NO_EXT_BASENAME=$(basename "${DYLIB_NO_EXT}")
+  DEST_DYLIB_NO_EXT="${DEST_PATH}/${DYLIB_NO_EXT_BASENAME}"
+  lipo -thin armv7 -output "${DEST_DYLIB_NO_EXT}.armv7" "${DYLIB}"
+  lipo -thin arm64 -output "${DEST_DYLIB_NO_EXT}.arm64" "${DYLIB}"
+  lipo "${DEST_DYLIB_NO_EXT}.armv7" "${DEST_DYLIB_NO_EXT}.arm64" -output "${DEST_DYLIB_NO_EXT}.dylib" -create
+  rm "${DEST_DYLIB_NO_EXT}.armv7" "${DEST_DYLIB_NO_EXT}.arm64"
 done
+
+rm -f "${DEST_PATH}/libswiftXCTest.dylib" 
+rm -f "${DEST_PATH}/libswiftWatchKit.dylib" 
